168d215ca2d95a346ba8134bca1f357a8df7f6ee
hamza wahed <hzd454@gmail.com>
2025-03-14 15:13:41 -0300

    return server instance and add test for health check endpoint

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,21 @@
-use Email-Newsletter::main;
+use email_newsletter::run;
 
-#[test]
-fn dummy_test() {
-    main()
+#[tokio::test]
+async fn health_check_works() {
+    spawn_app();
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() {
+    let server = run().expect("Failed to bind address");
+    let _ = tokio::spawn(server);
 }
